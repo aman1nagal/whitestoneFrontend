983cc822aa1d0385f8f26be20911c58c6f85a90c
aman1nagal <amanagrawal427@gmail.com>
2024-09-15 18:28:59 +0530

    minor changes

--- slices/socket.js
+++ slices/socket.js
@@ -1,6 +1,5 @@
 import { io } from "socket.io-client";
 
 // Initialize a socket connection
-const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL); // Make sure to set this in your environment file
-
+const socket = io(process.env.NEXT_PUBLIC_API_URL);
 export default socket;
